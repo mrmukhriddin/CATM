b4e087fb0702e966f329a484e9a9a8486cbf856f
mrMukhriddin <mrsiddikovmukhriddin@gmail.com>
2024-05-22 20:16:01 +0500

    add agree data ui module

---
+++ sources/feature/feature-agree-data-ui/src/main/java/ru/metasharks/catm/sources/feature/agree/data/ui/AgreeDataActivity.kt
@@ -0,0 +1,12 @@
+package ru.metasharks.catm.sources.feature.agree.data.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.metasharks.catm.feature.agree.data.ui.R
+
+class AgreeDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agree_data)
+    }
+}
---
+++ sources/feature/feature-agree-data-ui/src/main/java/ru/metasharks/catm/sources/feature/agree/data/ui/di/AgreeDataUiModule.kt
@@ -0,0 +1,11 @@
+package ru.metasharks.catm.sources.feature.agree.data.ui.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class AgreeDataUiModule {
+
+}
